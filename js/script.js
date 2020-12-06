6efb71c4e0ef6e01a1605d7a6315a5625692b1d6
mariaconejo <mary03081@gmail.com>
2020-12-05 21:26:29 -0600

    Se agrego html y fetch

--- js/script.js
+++ js/script.js
@@ -2,3 +2,36 @@
 // JS2 Project template.
 //
 
+const form = document.getElementById('form');
+const input = document.getElementById('search');
+const lista = document.getElementById('list');
+const info = document.getElementById('info');
+
+function addCountries(values) {
+  for (let i = 0; i < values.length; i += 1) {
+    const item = document.createElement('li');
+    info.appendChild(item);
+    fetch('https://restcountries.eu/rest/v2/all')
+      .then((response) => response.json())
+      .then((data) => {
+        const contenedor = `
+            <div id="modal">
+              <div id="info">
+                <img src="${data.flag}" alt="${data.name}">
+                <div id="detalles">
+                  <p>País: ${data.name} </p>
+                  <p>Capital: ${data.capital} </p>
+                  <p>Región: ${data.region} </p>
+                  <p>Subregión: ${data.subregion} </p>
+                  <p>Población  ${data.region} </p>
+                  <p>Área: ${data.area}  </p>
+                  <p>Idiomas: ${data.languages} </p>
+                  <p>Monedas: ${data.currencies} </p>
+                </div>
+              </div>
+            </div>
+          `;
+        item.innerHTML = contenedor;
+      });
+  }
+}
